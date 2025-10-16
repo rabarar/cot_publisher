81e67611f6b7fb4d32f33411bb12e35e47c8fef7
Rob Baruch <robertbaruch@mac.com>
2025-10-16 10:46:32 -0400

    first take

--- src/keys/mod.rs
+++ src/keys/mod.rs
@@ -6,10 +6,20 @@
 use pkcs8::{DecodePrivateKey, Error, PrivateKeyInfo, der::Encode};
 use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 
+use openssl::pkcs12::Pkcs12;
+use openssl::pkey::PKey;
+use openssl::pkey::Private;
+use openssl::stack::Stack;
+use openssl::x509::X509;
+use std::fs::File;
+use std::io::Read;
+
+
 /// Source for PEM file data
 pub enum Source {
     None,
     File(String),
+    FileP12(String, String),
     String(String),
 }
 
@@ -19,6 +29,7 @@ impl Source {
         match self {
             Source::None => Ok(String::new()),
             Source::File(path) => std::fs::read_to_string(path),
+            Source::FileP12(path, passwd) => read_pkcs12_to_string(path, passwd),
             Source::String(content) => Ok(content.clone()),
         }
     }
@@ -160,4 +171,63 @@ pub fn parse_certificates<'a>(cert_pem: String) -> Result<Vec<CertificateDer<'a>
         certs.push(cert_result?);
     }
     Ok(certs)
+}
+
+// -----
+
+pub fn read_pkcs12_to_string(
+    pkcs12_filename: &str,
+    pkcs12_passwd: &str,
+) -> Result<String, std::io::Error> {
+
+    // Load the PKCS#12 file
+    let mut file = File::open(&pkcs12_filename)
+        .expect(format!("Failed to open PKCS12 file: {}", pkcs12_filename).as_str());
+
+    let mut pkcs12_data = Vec::new();
+    file.read_to_end(&mut pkcs12_data)
+        .expect("Failed to read PKCS12 file");
+
+    // Load the identity using the PKCS#12 file and password
+    let parsed = Pkcs12::from_der(&pkcs12_data)?
+        .parse2(&pkcs12_passwd)
+        .expect(format!("Failed to load identity: {}", pkcs12_filename).as_str());
+
+    // These are accessible:
+    let leaf_cert:Option<X509> = parsed.cert; // openssl::x509::X509
+    let private_key:Option<PKey<Private>> = parsed.pkey; // openssl::pkey::PKey
+    let ca_chain:Option<Stack<X509>> = parsed.ca; // Option<Vec<openssl::x509::X509>>
+
+    let cert_pem_string:String;
+    let mut ca_cert_pem_string:String;
+    let pkey_pem_string:String;
+
+    if let Some(pkey) = private_key {
+        let pkey_pem_bytes = pkey.private_key_to_pem_pkcs8()?;
+        pkey_pem_string = String::from_utf8(pkey_pem_bytes)
+            .expect("failed to convert to utf8");
+    } else {
+        pkey_pem_string = "".to_string();
+    }
+        
+    if let Some(cert) = leaf_cert {
+        let cert_pem_bytes = cert.to_pem()?;
+        cert_pem_string = String::from_utf8(cert_pem_bytes)
+            .expect("failed to convert to utf8");
+    } else {
+        cert_pem_string = "".to_string();
+    }
+
+    if let Some(ca) = ca_chain {
+        ca_cert_pem_string = "".to_string();
+        for cert in ca.into_iter() {
+            let ca_cert_pem_bytes = cert.to_pem()?;
+            ca_cert_pem_string = String::from_utf8(ca_cert_pem_bytes)
+                .expect("failed to convert to utf8");
+        }
+    } else {
+        ca_cert_pem_string = "".to_string();
+    }
+
+    Ok([cert_pem_string, ca_cert_pem_string, pkey_pem_string].concat())
 }
